043574d67f28609df57764c232a1fc2555c885c0
kristobalus <ildar.tamendarov@gmail.com>
2024-06-08 01:06:10 +0500

    update

--- api/src/spelling-service.spec.ts
+++ api/src/spelling-service.spec.ts
@@ -1,19 +1,42 @@
 import assert from "assert"
 
-import companies from "./data/companies.json";
 import { SpellingService } from "./spelling-service";
+import { Company } from "./company";
 
 describe('SpellingService', () => {
+
+    const companies: Company[] = [
+        {
+            id: 1,
+            coins: [ "e1", "e2", "e3" ],
+            name: "company",
+            logoUrl: "url"
+        },
+        {
+            id: 2,
+            coins: [ "e1", "e2", "e3" ],
+            name: "group",
+            logoUrl: "url"
+        }
+    ]
+
     // eslint-disable-next-line @typescript-eslint/require-await
-    it('should filter by misplaced char', async () => {
+    it('should filter by 1 missed char', async () => {
         const service = new SpellingService(companies)
-        const result = await service.search("graup", 100, 0.5)
+        const result = await service.search("grop", 100, 0.5)
         assert(result.length)
     })
 
-    it('should filter with missed char', async () => {
+    it('should filter by 1 misplaced char', async () => {
         const service = new SpellingService(companies)
-        const result = await service.search("goup", 100, 0.5)
+        const result = await service.search("gorup", 100, 0.5)
         assert(result.length)
+    })
+
+    it('should filter with 2 or more missed char', async () => {
+        const service = new SpellingService(companies)
+        const [ company ] = await service.search("cmp", 100, 0.5)
+        assert(company)
+        assert.equal(company.name, companies[0].name)
     })
 })
